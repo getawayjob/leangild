9c2dc745748533fe05bcceafe9990e918386658f
rkcudjoe <rkcudjoe1@gmail.com>
2012-12-23 00:52:32 -0500

    deploying to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Leangild::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'leangild.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'leangild.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
 	address: "smtp.gmail.com",
