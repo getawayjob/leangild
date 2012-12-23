b3263e0147727d8d09cc68e6deb2f3a00f457d96
rkcudjoe <rkcudjoe1@gmail.com>
2012-12-23 00:57:04 -0500

    heroku issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Leangild::Application.configure do
   config.action_mailer.smtp_settings = {
 	address: "smtp.gmail.com",
 	port: 587,
-	domain: "leangild.heroku.com",
+	domain: "leangild.herokuapp.com",
 	authentication: "plain",
 	enable_starttls_auto: true,
 	user_name: ENV["GMAIL_USERNAME"],
