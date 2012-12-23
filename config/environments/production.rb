52c065b4e046de5e5411f11643755093ff78f92e
rkcudjoe <rkcudjoe1@gmail.com>
2012-12-22 23:12:41 -0500

    config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,16 @@ Leangild::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'leangild.com' }
+  config.action_mailer.default_url_options = { :host => 'leangild.heroku.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+	address: "smtp.gmail.com",
+	port: 587,
+	domain: "leangild.heroku.com",
+	authentication: "plain",
+	enable_starttls_auto: true,
+	user_name: ENV["GMAIL_USERNAME"],
+	password: ENV["GMAIL_PASSWORD"]
+  }
+
 end
