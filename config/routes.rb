31b587456b3bc8b26fd5e036958b128a1e785171
rkcudjoe <rkcudjoe1@gmail.com>
2012-12-30 00:19:57 -0500

    Deleted rsocialize files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Leangild::Application.routes.draw do
-  match '/sharrre' => 'Rsocialize#sharrre'
+  
   
   resources :invitations, only: [:create, :destroy]
   match 'bulk_invite', :to => 'invitations#bulk_invite'
