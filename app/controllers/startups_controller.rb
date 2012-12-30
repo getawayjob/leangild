eb0ace2332b3d6ad68d33ca342a4c24fdd484dd1
rkcudjoe <rkcudjoe1@gmail.com>
2012-12-30 05:36:23 -0500

    Some fixes

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -1,5 +1,5 @@
 class StartupsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:index, :edit, :destroy, :update]
   load_and_authorize_resource :only => [:index, :edit, :destroy, :update]
 
   # GET /startups
