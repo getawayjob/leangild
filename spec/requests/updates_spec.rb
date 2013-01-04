b5621c584819ae3c14c9328337f539598f80e440
rkcudjoe <rkcudjoe1@gmail.com>
2013-01-04 18:35:35 -0500

    Few updates, w/spec

--- spec/requests/updates_spec.rb
+++ spec/requests/updates_spec.rb
@@ -3,8 +3,17 @@ require 'spec_helper'
 describe "Updates" do
   let(:user) { FactoryGirl.create(:user) }
   let(:startup) { FactoryGirl.create(:startup, user: user) }
-
-  #No spec written
-  
-  #Only the owner and admin can send updates
+  before do
+	sign_in(user)
+	visit ( startup_path(startup))
+  end
+ 
+  describe "Posting an update" do
+  	before do
+	fill_in "Progress Update", with: "Progess"
+  end
+  it "should post an update" do
+	expect { click_button "Post Update" }.to change(Update, :count).by(1)
+   end
+ end	
 end
