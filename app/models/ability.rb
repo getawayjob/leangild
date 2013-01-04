59c12cc0f5588002d0eab9f507f65fb11cb7638a
rkcudjoe <rkcudjoe1@gmail.com>
2013-01-04 01:48:40 -0500

    Added ability to delete

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,17 +14,17 @@
         	startup.try(:user) == user
         end
         can :destroy, Startup do |startup|
-        	startup.try(:user) == user
+        	startup.try(:user) == Startup.user
         end
         can :bulk_invite, Invitation do |invite|
 		    invite.try(:user) == Startup.user
        end
-   #     can :create, Update do |update|
-			# update.try(:user) == Update.startup.user
-   #     end
-   #     can :destroy, Update do |update|
-			# update.try(:user) == Update.startup.user
-   #     end
+       can :create, Update do |update|
+			update.startup.user == user
+       end
+       can :destroy, Update do |update|
+			update.startup.user == user
+       end
       end
       
     # The first argument to `can` is the action you are giving the user permission to do.
